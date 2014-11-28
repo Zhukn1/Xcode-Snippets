516f28668f52e031de3dedc5c9ddb760442f2f6d
Stas Zhukovskiy <stzhuk@gmail.com>
2014-11-28 16:14:08 +0300

    added pragma mark snippet

---
+++ pragma_mark.m
@@ -0,0 +1,11 @@
+// Pragma mark
+// 
+//
+// IDECodeSnippetCompletionPrefix: pragma
+// IDECodeSnippetCompletionScopes: [ClassImplementation]
+// IDECodeSnippetIdentifier: EC956108-24DC-4155-B169-32038987B2FD
+// IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Objective-C
+// IDECodeSnippetUserSnippet: 1
+// IDECodeSnippetVersion: 0
+
+#pragma mark - <#Name#>
