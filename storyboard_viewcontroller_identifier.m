66c8ca27c211ccb0bf87f7fad804f63a57d4433e
Stas Zhukovskiy <stzhuk@gmail.com>
2014-12-07 23:10:26 +0300

    Imported user code snippets

---
+++ storyboard_viewcontroller_identifier.m
@@ -0,0 +1,12 @@
+// StoryBoard ViewController Identifier
+// Use this method instead of direct string declaration.
+//
+// IDECodeSnippetCompletionPrefix: storyBoardIdentifier
+// IDECodeSnippetCompletionScopes: [ClassImplementation]
+// IDECodeSnippetIdentifier: EFDE590D-FB94-4074-BC3F-D7B21FAA9E03
+// IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Objective-C
+// IDECodeSnippetUserSnippet: 1
+// IDECodeSnippetVersion: 2
++ (NSString *)storyBoardIdentifier {
+    return NSStringFromClass(self);
+}
