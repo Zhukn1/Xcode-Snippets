bad44f9bd65c56b318304a34234c2bf94340534f
Stas Zhukovskiy <stzhuk@gmail.com>
2014-12-08 15:26:28 +0300

    Update documents.m

--- documents.m
+++ documents.m
@@ -2,7 +2,6 @@
 // 
 //
 // IDECodeSnippetCompletionScopes: [All]
-// IDECodeSnippetIdentifier: 3298D7C1-4423-429C-B4E3-F0EA65B8ABEA
 // IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Objective-C
 // IDECodeSnippetUserSnippet: 1
 // IDECodeSnippetVersion: 2
