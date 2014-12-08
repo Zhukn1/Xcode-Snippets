3be5ade75260a86dc02ba857aac37d622bb0ddbc
Stas Zhukovskiy <stzhuk@gmail.com>
2014-12-08 15:26:41 +0300

    Update pragma_mark.m

--- pragma_mark.m
+++ pragma_mark.m
@@ -3,7 +3,6 @@
 //
 // IDECodeSnippetCompletionPrefix: pragma
 // IDECodeSnippetCompletionScopes: [ClassImplementation]
-// IDECodeSnippetIdentifier: EC956108-24DC-4155-B169-32038987B2FD
 // IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Objective-C
 // IDECodeSnippetUserSnippet: 1
 // IDECodeSnippetVersion: 2
