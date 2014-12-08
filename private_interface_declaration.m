e71e839d732c3ec66344583ad6123611b14d408d
Stas Zhukovskiy <stzhuk@gmail.com>
2014-12-08 15:32:07 +0300

    Update private_interface_declaration.m

--- private_interface_declaration.m
+++ private_interface_declaration.m
@@ -6,6 +6,6 @@
 // IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Objective-C
 // IDECodeSnippetUserSnippet: 1
 // IDECodeSnippetVersion: 2
-@interface CreditCardCell ()
+@interface <#Class Name#> ()
 
 @end
