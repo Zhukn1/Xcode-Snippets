ac849f06ee06b8312b56a08ea62f7c08a2f6ac6e
Stas Zhukovskiy <stzhuk@gmail.com>
2014-12-08 15:26:55 +0300

    Update private_interface_declaration.m

--- private_interface_declaration.m
+++ private_interface_declaration.m
@@ -3,7 +3,6 @@
 //
 // IDECodeSnippetCompletionPrefix: interface
 // IDECodeSnippetCompletionScopes: [All]
-// IDECodeSnippetIdentifier: B039A835-AD8C-4DD0-AAC9-B20972BA05A9
 // IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Objective-C
 // IDECodeSnippetUserSnippet: 1
 // IDECodeSnippetVersion: 2
